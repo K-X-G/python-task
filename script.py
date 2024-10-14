0489b815f68a2c1d573d4e98067a5e424831fbdb
Georgiev, Kristian (I/BT-O54, extern) <georgiev@hpe.com>
2024-10-14 14:26:38 +0300

    added API request

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import logging
 import logging.config
 import yaml
+import requests
 
 # Task 1: Read from configuration file
 # logging.config.fileConfig('logging.conf')
@@ -14,9 +15,13 @@ logger = logging.getLogger('staging')
 
 # Main
 # Task 2: Gather data from API
+ip_address = input("Please enter the ip address: ")
+url = f"https://ipapi.co/{ip_address}/json/"
+loc = requests.get(url)
+print(loc)
 
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warning message')
-logger.error('error message')
-logger.critical('critical message')
+# logger.debug('debug message')
+# logger.info('info message')
+# logger.warning('warning message')
+# logger.error('error message')
+# logger.critical('critical message')
